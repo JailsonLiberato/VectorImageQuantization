d290e50178653c9a32149ef4dcfe0dc541ed4bc8
Jailson Liberato <jailsonliberato@localhost.localdomain>
2018-12-11 00:13:26 -0300

    Apresentação projeto CI finalizado.

--- Main.py
+++ Main.py
@@ -1,22 +1,18 @@
 #coding: utf-8
-import cv2
-import numpy as np
-import pyswarms as ps
-from pyswarms.utils.functions import single_obj as fx
 from JTools import JToolsClass
 from Kmeans import KmeansClass
 
 class MainClass:
     
-    jtools = JToolsClass()
-    kmeans = KmeansClass()
+    __jtools = JToolsClass()
+    __kmeans = KmeansClass()
 
     def __init__(self):
-        self.jtools.clear()
-        self.executar()
+        self.__jtools.clear()
+        self.__executar()
 
-    def executar(self):
+    def __executar(self):
         print("\n\n\t\t\t\t\t::::Quantização Vetorial de Imagem::::")
-        self.kmeans.executar("imagens/globo.jpg")
+        self.__kmeans.executar("imagens/globo.jpg")
 
 main = MainClass()   
--- PSO.py
+++ PSO.py
@@ -15,21 +15,13 @@ class PSOClass:
 
     kmeans = KMeans()
     image = Image.open("imagens/globo.jpg")
-    image = np.array(image, dtype=np.float64) / 255
+    image = np.array(image, dtype=np.float64) # / 255
     #Propriedades da imagem 
     width, height, depth = tuple(image.shape)
     image_array = np.reshape(image, (width * height, depth))
     image_array_amostra = shuffle(image_array, random_state=0)[:2]
 
-    def fitness(self, x, y):
-        #kmeans = KMeans(n_clusters=64, n_init=1, init=self.image_array_amostra).fit(self.image_array)
-        #print(kmeans.labels_)
-        ##silhouette_avg = silhouette_score(self.image_array, kmeans.labels_)
-        #print(silhouette_avg)
-        #return np.linalg.norm(self.image_array - self.image_array_amostra, axis=-1).sum()
-        #kmeans = KMeans(n_clusters=2,init=self.image_array_amostra).fit(self.image_array)
-        #silh=(-1)*silhouette_score(self.image_array,kmeans.labels_)
-        #return silh
+    def __fitness(self, x, y):
         coeffs=[]
         kmeans = KMeans(n_clusters=64).fit(self.image_array)
         sil_coeff = silhouette_score(self.image_array, kmeans.labels_,metric='euclidean')
@@ -39,9 +31,10 @@ class PSOClass:
         return coeffs
     
     def execute(self, array):
-        options = {'c1': 0.9, 'c2': 0.9, 'w':1.9}
-        optimizer = ps.single.GlobalBestPSO(n_particles=30, dimensions=3, options=options)
-        cost, pos = optimizer.optimize(self.fitness, 1, print_step=1, verbose=3, y=1)
+        options = {'c1': 0.5, 'c2': 0.5, 'w':1.0}
+        print(array)
+        optimizer = ps.single.GlobalBestPSO(n_particles=1000, dimensions=64, options=options)
+        cost, pos = optimizer.optimize(self.__fitness, 1 , print_step=1, verbose=3, y=1)
         print(cost)
         print(pos)
         return pos
